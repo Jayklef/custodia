9ccf828a9aa78b74ef37adbbfad4d77a636ca2c1
jerry-spinto <jerry@woodcoreapp.com>
2022-02-27 23:40:40 +0100

    Added HttpBasic security Configuration

--- src/main/java/com/jayklef/custodia/config/WebSecurityConfiguration.java
+++ src/main/java/com/jayklef/custodia/config/WebSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.jayklef.custodia.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,5 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
 }
